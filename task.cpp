9123fe97e2a7ddd2b5dfe27347dddeb8b4dfc454
Heewon <heewon@example.com>
2024-12-15 17:31:19 +0900

    차량소프트웨어 과제

---
+++ task.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "。° 。 ° ˛ ˚ ˛ ˚ ˛ ·˚" << std::endl;
+    std::cout << "· 。 ° · 。 · ˚ ˚ ˛ ˚ ˛" << std::endl;
+    std::cout << "。° 。 ° 。˚ ˛ · ˚ ˚ ˛" << std::endl;
+    std::cout << "*★MERRY★* 。 · ˚ ˚ ˛ ˚ ˛ · ·" << std::endl;
+    std::cout << "。CHRISTMAS 。* 。° 。 ° ˛ ˚ ˛" << std::endl;
+    std::cout << "* _Π___＿*。*˚ ˚ ˛ ˚ ˛ ·˛ ·˚" << std::endl;
+    std::cout << "*/____＿/~＼。˚ ˚ ˛ ˚ ˛ ·˛ ·˚" << std::endl;
+    std::cout << "｜ 田田 ｜門｜ ˚ ˛ ˚ ˛ ·" << std::endl;
+    std::cout << "━○━★‥…+->♡<-+…‥★━○━♬" << std::endl;
+
+    return 0;
+}
